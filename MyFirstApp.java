f41727c13b38c8a9edf48da663b384a8ad16e63c
Prosper <pntigurirwa@gmail.com>
2019-11-07 00:28:06 -0600

    Statements, Looping, Branching

---
+++ MyFirstApp.java
@@ -0,0 +1,32 @@
+public class MyFirstApp{
+
+    public static void main(String[] args){
+	int x = 3;
+	String name ="dirk";
+	x=x*17;
+	System.out.println("x is" + x);
+	double d = Math.random();
+	// this is a comment
+	//do SOmething gain and again 
+	while  (x>12){
+	    x=x-1;
+	    
+}
+	for (int y=0; y>10; y =y+1){
+	    System.out.println("y is now" +y);
+}
+	// Do something uder this condition 
+	if (x== 10)
+	    {
+		System.out.println("x isn't 10");
+	    }
+	if((x>3)&(name.equals("dirk")))
+	    {
+		System.out.println("Gently");
+	    }
+	System.out.println("this line runs no matter what");
+	System.out.println("I rule!");
+	System.out.println("The world");
+
+}
+}
