e07c220ece5539d0a43b7d5355d6af1442fa354c
pntigurirwa <pntigurirwa@gmail.com>
2020-03-30 13:12:10 -0500

    inputing and outputing floating numbers with Account objects

--- ATM/Account.java
+++ ATM/Account.java
@@ -3,12 +3,30 @@
 
 public class Account {
     private String name; // instance variable
+    private double balance; //instance variable
 
-    public Account (String name)// constructor name is class name
+    // Account constructor that receives two parameters
+    public Account (String name, double balance)
     {
-        this.name = name;
+        this.name = name; //Assign name to instance variable name 
+        
+        //validate that the balance is gretaer tahn 0.0; if it's not, 
+        //instance variable balance keeps ists default initial value of 0.0
+        if(balance  > 0.0) //if balance is valid
+        this.balance = balance; // assign it to instance variable balance 
+    }
+    
+    //method that deposits(adds) only a valid amount to the balance 
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0) // if depositAmount is valid
+            balance =balance + depositAmount; //Add it to the balance 
     }
 
+    // Method returns the account balance 
+    public double getBalance(){
+        return balance;
+    }
+    
     //Method to set name in the object
     public void setName(String name){
         this.name = name; //store the name 
--- ATM/AccountTest.java
+++ ATM/AccountTest.java
@@ -2,14 +2,41 @@
 //Using thre Account constructor to initialize the name instance
 //variable at the time each Account object is created
 
+
+//Inputing and outputing floating point numbers with Account objects
+
+import java.util.Scanner; 
+
 public class AccountTest{
     public static void main(String[] args) {
         //Creaete two Accounts objects
-        Account account1 = new Account ("Jane Green");
-        Account account2 = new Account ("Jane Blue");
+        Account account1 = new Account ("Jane Green", 50.00);
+        Account account2 = new Account ("Jane Blue", -7.53);
 
         // display initial value of name for each  Account 
-        System.out.printf("Account1 name is : %s%n", account1.getName());
-        System.out.printf("Account2 name is : %s%n", account2.getName());
+        System.out.printf("%s balance: $%.2f%n%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n%n", account2.getName(), account2.getBalance());
+
+        //create a scanner to obtain input from the command window
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Enter Deposit amount for account1: ");// prompt 
+        double depositAmount = input.nextDouble(); //obtain user input
+        System.out.printf("nadding %.2f to account1 balance%n%n", depositAmount);// add to account1's balance 
+
+        //Display balances 
+        System.out.printf("%s balance : $%.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance : $%.2f%n", account2.getName(), account2.getBalance());
+
+        System.out.print("Enter Deposit amount for account1: ");// prompt 
+        depositAmount = input.nextDouble(); //obtain user input
+        System.out.printf("nadding %.2f to account2 balance%n%n", depositAmount);// add to account2's balance 
+
+        //Display balances 
+        System.out.printf("%s balance : $%.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance : $%.2f%n", account2.getName(), account2.getBalance());
+
+
+
     }
 }// end of class AccountTEst
