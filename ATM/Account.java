6491342a403cc8d1a6da3dfd102a09c4dccda8fa
pntigurirwa <pntigurirwa@gmail.com>
2020-03-30 12:14:25 -0500

    Contsructor declaration

--- ATM/Account.java
+++ ATM/Account.java
@@ -4,6 +4,11 @@
 public class Account {
     private String name; // instance variable
 
+    public Account (String name)// constructor name is class name
+    {
+        this.name = name;
+    }
+
     //Method to set name in the object
     public void setName(String name){
         this.name = name; //store the name 
--- ATM/AccountTest.java
+++ ATM/AccountTest.java
@@ -1,26 +1,15 @@
 // Creatinf and manipulating an account object
-
-import java.util.Scanner;
+//Using thre Account constructor to initialize the name instance
+//variable at the time each Account object is created
 
 public class AccountTest{
     public static void main(String[] args) {
-        // cretae a scanner object to obtain input from the command window
-        Scanner input = new Scanner(System.in);
+        //Creaete two Accounts objects
+        Account account1 = new Account ("Jane Green");
+        Account account2 = new Account ("Jane Blue");
 
-        //create and account object and assign it to myAccount
-        Account myAccount = new Account();
-
-        //display initial value of name (null)
-        System.out.printf("initial name is %s%n%n", myAccount.getName());
-
-        //prompt for and read name 
-        System.out.println("please enter the name: ");
-        String theName = input.nextLine(); // read a line of text
-        myAccount.setName (theName); // put theName in myAccount
-        System.out.println();// output a blank line
-
-        //display the name stored in object myAccount
-        System.out.printf("name in object myAccount is : %n%s%n", myAccount.getName());
-
+        // display initial value of name for each  Account 
+        System.out.printf("Account1 name is : %s%n", account1.getName());
+        System.out.printf("Account2 name is : %s%n", account2.getName());
     }
 }// end of class AccountTEst
