c106987b40f92ff7758d1e485c625c73003f8934
pntigurirwa <pntigurirwa@gmail.com>
2020-03-30 13:56:24 -0500

    Using Dialog Box

---
+++ ATM/Dialog1.java
@@ -0,0 +1,10 @@
+// Using JoptionPane to display multiple lines in a dialog box
+
+import javax.swing.JOptionPane;
+
+public class Dialog1{
+     public static void main(String[] args) {
+    // Display a dialog with a message 
+    JOptionPane.showMessageDialog(null, "welcome to Java");    
+    }
+}// End of class Dialog 
