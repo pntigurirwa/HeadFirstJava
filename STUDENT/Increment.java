f982efa71edaf89d5eb45885674c379f749575ce
pntigurirwa <pntigurirwa@gmail.com>
2020-04-01 14:01:54 -0500

    good programming practice

--- STUDENT/Increment.java
+++ STUDENT/Increment.java
@@ -15,3 +15,12 @@ public class Increment{
         System.out.printf("c after preincrement : %d%n", c); //print 6
     }
 }// end class Increment 
+
+/* Good Prgramming Practice: 
+When writting expressions containing many operators confirm that the operators
+in the expression are performed in the order you expect. If you are uncertain
+about the order of evaluation in a complex expression, braeak the expression into 
+smaller statements or use parentheses to force the order of evaluation, exactly 
+as you'd do in an algebraic expression. be sure to ibserve that some operators 
+such as assignement(=) associate right to left rather than left to right 
+*/
