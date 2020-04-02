b9460a6f39151aab04dfe02498e50a8a90107d36
pntigurirwa <pntigurirwa@gmail.com>
2020-04-02 14:25:16 -0500

    variable declaration '

---
+++ CONTROL_STATEMENTS/LetterGrades.java
@@ -0,0 +1,15 @@
+//Letter grades class uses the Switch statement to count letter grades 
+
+import java.util.Scanner;
+public class LettterGrades{
+    public static void main(String[] args) {
+        
+        int total = 0;// sum of grades 
+        int gradeCounter = 0; // number of grades entered 
+        int aCount = 0; //Count of A grades 
+        int bCount =0; // comut of B grades
+        int cCount = 0; // Count of C count 
+        int dCount = 0; //Count of D count 
+        int fCount =0; // Count of F Grades 
+    }
+}
