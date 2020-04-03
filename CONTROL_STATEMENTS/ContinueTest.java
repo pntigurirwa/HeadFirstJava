490cabaad54c3f6a56c0db738856e379d5e33e51
pntigurirwa <pntigurirwa@gmail.com>
2020-04-03 12:39:14 -0500

    continue to skip printing a number

---
+++ CONTROL_STATEMENTS/ContinueTest.java
@@ -0,0 +1,14 @@
+// break statement exiting a for statement
+public class ContinueTest
+{
+    public static void main(String[] args) {
+        int count ; // control variable also used after loop terminates 
+        for (count = 1; count <= 10; count ++){
+            if(count == 5)
+            continue; //skip remaining code in loop if count is 5
+
+            System.out.printf("%d", count);
+        }
+        System.out.printf("%nUsed continue to skip printing 5%d%n");
+    }
+}//end class Breaktest
