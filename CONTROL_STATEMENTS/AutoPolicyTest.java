d20ffd17ad45687c3cc7495df44cbcd33cd9cf74
pntigurirwa <pntigurirwa@gmail.com>
2020-04-03 11:53:33 -0500

    method to display whether Autopolicy

--- CONTROL_STATEMENTS/AutoPolicyTest.java
+++ CONTROL_STATEMENTS/AutoPolicyTest.java
@@ -5,5 +5,12 @@ public class AutoPolicyTest
         // create two Autopolicy objects
         AutoPolicy policy1 = new AutoPolicy (11111111, "Toyota Camry", "NJ"); 
         AutoPolicy policy1 = new AutoPolicy (22222222, "Ford Fusion", "ME");
+
+        // display whether  each policy is in a no fault state
+        policyInNofaultState(policy1);
+        policyInNofaultState(policy2);
     }
+
+    //method that display whether 
+    
 }
