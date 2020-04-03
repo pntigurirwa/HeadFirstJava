c3ac960a50b31bfd0c124f0a6ffe0cc735dfb862
pntigurirwa <pntigurirwa@gmail.com>
2020-04-03 12:33:44 -0500

    broke out of loop at count

--- CONTROL_STATEMENTS/BreakTest.java
+++ CONTROL_STATEMENTS/BreakTest.java
@@ -1,1 +1,14 @@
 // break statement exiting a for statement
+public class BreakTest
+{
+    public static void main(String[] args) {
+        int count ; // control variable also used after loop terminates 
+        for (count = 1; count <= 10; count ++){
+            if(count == 5)
+            break; //Terminate loop if count is 5
+
+            System.out.printf("%d", count);
+        }
+        System.out.printf("%nBroke out of loop at count = %d%n", count);
+    }
+}//end class Breaktest
