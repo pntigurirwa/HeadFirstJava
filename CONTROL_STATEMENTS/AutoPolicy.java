a870161f1c154b51c6499d7f46097241753bbced
pntigurirwa <pntigurirwa@gmail.com>
2020-04-03 11:17:44 -0500

    Auto insurance policy

---
+++ CONTROL_STATEMENTS/AutoPolicy.java
@@ -0,0 +1,29 @@
+// Class that represents an auto insurance policy
+
+public class AutoPolicy{
+    private int accountNumber;// policy account number 
+    private String makeAndModel;// car that the policy applies to
+    private String state; // two letter state abbreviation 
+
+    //Constructor 
+    public AutoPolicy (int accountNumber, String makeAndModel, String state)
+    {
+        this.accountnumber = accountNumber; 
+        this.makeAndModel = makeAndModel; 
+        this.state = state; 
+    }
+
+    //sets the accountNumber 
+    public void setAccountNumber(int accountNumber)
+    {
+        this.accountNumber = accountnumber;
+    }
+
+    //returns the accountNumber 
+    public int getAccountNumber ()
+    {
+        return accountnumber; 
+    }
+
+    // 
+}
