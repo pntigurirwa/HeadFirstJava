9d211e30a2d835cca98a69b2bc743aa5ba348ade
pntigurirwa <pntigurirwa@gmail.com>
2020-04-03 12:11:47 -0500

    Testing

--- CONTROL_STATEMENTS/AutoPolicyTest.java
+++ CONTROL_STATEMENTS/AutoPolicyTest.java
@@ -11,6 +11,14 @@ public class AutoPolicyTest
         policyInNofaultState(policy2);
     }
 
-    //method that display whether 
-    
-}
+    //method that display whether an Autopolicy 
+    // is in a state with no fault auto insurance 
+    public static void policyInNoFaultState(AutoPolicy policy){
+        System.out.println("The auto policy: ");
+        System.out.printf("Account#: %d; Car: %s; State %s %s a no-fault stae%n%n", 
+        policy.getAccountNumber(), policy.getMakeAndModel(), policy.getState(), 
+        policy.getState(), 
+        (policy.isNoFaultState() ? "is": "is not"));
+    }
+
+}// end class AutoPolicy
