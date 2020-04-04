8a200e7904b7482d13ce5a22a26822e7f7fbedd9
pntigurirwa <pntigurirwa@gmail.com>
2020-04-04 17:38:25 -0500

    truth table for boolean logical OR

--- CONTROL_STATEMENTS/LogicalOperators.java
+++ CONTROL_STATEMENTS/LogicalOperators.java
@@ -11,9 +11,24 @@ public static void main(String[] args) {
     // Create truth for || conditional oprerator 
     System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%n", 
     "Conditional OR (||)", "false || false", (false || false), 
-    "false && true", (false || true),
-    "true && false", (true || false),
-    "true && true", (true || true ));
+    "false || true", (false || true),
+    "true || false", (true || false),
+    "true || true", (true || true ));
 
-    
+    // Create truth for & (boolean logical AND) oprerator 
+    System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%n", 
+    "Conditional AND (&)", "false & false", (false & false), 
+    "false & true", (false & true),
+    "true & false", (true & false),
+    "true & true", (true & true ));
+
+    // Create truth for | (boolean logical OR) oprerator 
+    System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%n", 
+    "Conditional OR (|)", "false & false", (false |false), 
+    "false | true", (false | true),
+    "true | false", (true | false),
+    "true | true", (true | true ));
+
+
+
 }
