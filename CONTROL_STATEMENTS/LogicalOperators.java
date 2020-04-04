92efe95dd1f412356df7ce646e9697d54ae1981d
pntigurirwa <pntigurirwa@gmail.com>
2020-04-04 17:31:48 -0500

    truth tbale for AND

---
+++ CONTROL_STATEMENTS/LogicalOperators.java
@@ -0,0 +1,10 @@
+//Logical Operators 
+
+public static void main(String[] args) {
+    // Create truth for && conditional oprerator 
+    System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%n", 
+    "Conditional AND (&&)", "false && false", (false && false), 
+    "false && true", (false && true),
+    "true && false", (true && false),
+    "true && true", (true && true ));
+}
