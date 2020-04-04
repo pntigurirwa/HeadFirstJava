6a912497c06f06e34e19c98fbdd84033afe927f8
pntigurirwa <pntigurirwa@gmail.com>
2020-04-04 17:57:32 -0500

    draw rectangle

---
+++ CONTROL_STATEMENTS/Shapes.java
@@ -0,0 +1,34 @@
+//drawing a cascade of shapes based on the user's choice 
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Shapes extends JPanel
+{
+    private int choice; // user's choice of which shape to draw
+
+    //Constructor sets the user's choice 
+    public  Shapes(int userChoice)
+    {
+        choice = userChoice;
+    }
+
+    //draw a cascade of shapes starting from the top left corner 
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponet(g);
+            for (int i= 0; i<10; i++) {
+                //Pick the shape based on the user's choice 
+                switch (choice) {
+                    case 1: //draw rectangles
+                        g.drawRect(10 + i * 10, 10 +i * 10, 50 + i * 10, 50 + i * 10);
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+                
+            }
+    }
+}
