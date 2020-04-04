f10f856bcc7310aeacb76f88fb028ccf79b84407
pntigurirwa <pntigurirwa@gmail.com>
2020-04-04 17:34:29 -0500

    truth tbale for OR

--- CONTROL_STATEMENTS/LogicalOperators.java
+++ CONTROL_STATEMENTS/LogicalOperators.java
@@ -7,4 +7,13 @@ public static void main(String[] args) {
     "false && true", (false && true),
     "true && false", (true && false),
     "true && true", (true && true ));
+
+    // Create truth for || conditional oprerator 
+    System.out.printf("%s%n%s: %b%n%s: %b%n%s: %b%n%s: %b%n%n", 
+    "Conditional OR (||)", "false || false", (false || false), 
+    "false && true", (false || true),
+    "true && false", (true || false),
+    "true && true", (true || true ));
+
+    
 }
