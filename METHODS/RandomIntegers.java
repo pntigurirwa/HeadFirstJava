433a92dd85631a718f3f84f08070ceef69309f9e
pntigurirwa <pntigurirwa@gmail.com>
2020-04-05 18:51:29 -0500

    pick a random integers

--- METHODS/RandomIntegers.java
+++ METHODS/RandomIntegers.java
@@ -5,6 +5,20 @@ import java.security.SecureRandom; //program uses class SecureRandom
 public class RandomIntegers
 {
     public static void main(String[] args) {
+        // randomNumbers objec will produce random nubers 
+        SecureRandom randomNumbers = new SecureRandom();
+
+        //loop 20 times 
+        for (int counter = 1; counter <= 20; counter ++) {
         
+            //pick random integer from 1 to 6 
+            int face = 1 + RandomNumbers.nextInt(6);
+
+            System.out.print("%d ", face); // display generated value 
+
+            //if counter is divisible by 5, start a new line of output 
+            if(counter % 5 == 0)
+            System.out.println();
+        }
     }
-}
+}//end class 
