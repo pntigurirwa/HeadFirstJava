ae523abaa3a6e4207937a9a0a026a6b028dc0110
pntigurirwa <pntigurirwa@gmail.com>
2020-04-05 18:46:12 -0500

    Random Integers

---
+++ METHODS/RandomIntegers.java
@@ -0,0 +1,10 @@
+// Shifted and scaled random integers
+
+import java.security.SecureRandom; //program uses class SecureRandom
+
+public class RandomIntegers
+{
+    public static void main(String[] args) {
+        
+    }
+}
